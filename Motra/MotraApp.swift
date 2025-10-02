e17042dc001b63033b3e35a976967e2c932dcd42
zaezae <jebyun2@gmail.com>
2025-10-03 02:34:32 +0900

    first commit

---
+++ Motra/MotraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotraApp.swift
+//  Motra
+//
+//  Created by Jaeeun Byun on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MotraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MotraTests/MotraTests.swift
@@ -0,0 +1,17 @@
+//
+//  MotraTests.swift
+//  MotraTests
+//
+//  Created by Jaeeun Byun on 10/3/25.
+//
+
+import Testing
+@testable import Motra
+
+struct MotraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
